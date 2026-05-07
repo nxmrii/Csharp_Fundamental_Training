49d938f80d2c6100f0d58b566cb581b1abc9e9d2
nxmrii <nalaamri19@gmail.com>
2026-05-07 13:09:20 +0400

    add csharp solution

---
+++ Csharp_Fundamental_Training/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_Fundamental_Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
